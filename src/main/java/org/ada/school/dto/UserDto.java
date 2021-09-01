fceae6abe72ed3a43313c1e97fe7b329b4d991db
Andres Calderon <andres.calderon@payu.com>
2021-09-01 09:22:36 -0500

    Lombok annotation processor added and documentation to all the class.

--- src/main/java/org/ada/school/dto/UserDto.java
+++ src/main/java/org/ada/school/dto/UserDto.java
@@ -1,29 +1,32 @@
 package org.ada.school.dto;
 
-public class UserDto
-{
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Mapping class for the User class
+ *
+ * @author Andres Calderon (andres.calderon@escuelaing.edu.co)
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+@NoArgsConstructor
+public class UserDto {
+
+    /**
+     * The name of the user
+     */
     String name;
 
+    /**
+     * The email of the user
+     */
     String email;
 
+    /**
+     * The lastName of the user
+     */
     String lastName;
 
-    public UserDto()
-    {
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public String getEmail()
-    {
-        return email;
-    }
-
-    public String getLastName()
-    {
-        return lastName;
-    }
 }
